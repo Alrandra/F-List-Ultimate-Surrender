b13e4ec5a47953237f3d7460540920231292e43f
Alrandra <treublatt@gmail.com>
2015-12-29 11:10:16 +0100

    Added global moves variable, primed with Brawl Moves @TODO add all other moves

---
+++ app/scripts/moves.js
@@ -0,0 +1,87 @@
+app.usamoves = [
+  {
+    name: 'SLAM!',
+    reqStrength: 5,
+    reqSpeed: 1,
+    reqOrgasm: 0,
+    reqSubmission:0,
+    lustDamage: 0,
+    physicalDamage: 15,
+    staminaCost: 15,
+    holdDuration: 0,
+    category: 'brawl',
+    decription: 'Slamming the opponent onto the mat and landing on top of them'
+  }, {
+    name: 'Clothesline',
+    reqStrength: 5,
+    reqSpeed: 1,
+    reqOrgasm: 0,
+    reqSubmission:0,
+    lustDamage: 0,
+    physicalDamage: 15,
+    staminaCost: 15,
+    holdDuration: 0,
+    category: 'brawl',
+    decription: 'Classic wrestling move of an arm to the neck! '
+  }, {
+    name: 'Dropkick',
+    reqStrength: 1,
+    reqSpeed: 5,
+    reqOrgasm: 0,
+    reqSubmission:0,
+    lustDamage: 0,
+    physicalDamage: 10,
+    staminaCost: 10,
+    holdDuration: 0,
+    category: 'brawl',
+    decription: 'Double kick to the chest or head of someone!'
+  }, {
+    name: 'Chops / Slaps',
+    reqStrength: 3,
+    reqSpeed: 3,
+    reqOrgasm: 0,
+    reqSubmission:0,
+    lustDamage: 0,
+    physicalDamage: 10,
+    staminaCost: 10,
+    holdDuration: 0,
+    category: 'brawl',
+    decription: 'Chop / Slap your opponent'
+  }, {
+    name: 'Punches',
+    reqStrength: 2,
+    reqSpeed: 2,
+    reqOrgasm: 0,
+    reqSubmission:0,
+    lustDamage: 0,
+    physicalDamage: 5,
+    staminaCost: 3,
+    holdDuration: 0,
+    category: 'brawl',
+    decription: 'Punch your opponent'
+  }, {
+    name: 'Kicks',
+    reqStrength: 2,
+    reqSpeed: 3,
+    reqOrgasm: 0,
+    reqSubmission:0,
+    lustDamage: 0,
+    physicalDamage: 8,
+    staminaCost: 5,
+    holdDuration: 0,
+    category: 'brawl',
+    decription: 'Kick your opponent'
+  }, {
+    name: 'Miscellaneous',
+    reqStrength: 1,
+    reqSpeed: 1,
+    reqOrgasm: 0,
+    reqSubmission:9,
+    lustDamage: 0,
+    physicalDamage: 15,
+    staminaCost: 10,
+    holdDuration: 2,
+    category: 'brawl',
+    decription: 'Kick your opponent'
+  }
+];
